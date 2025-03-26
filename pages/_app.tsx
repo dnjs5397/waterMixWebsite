c5b5154270636856996b57f5e3362e214752f2e5
Seungwon <dnjs5397@naver.com>
2025-03-27 00:46:42 +0900

    네이버 서치어드바이저 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="naver-site-verification" content="abcd1234efgh5678" />
+        <meta name="naver-site-verification" content="91c08553d77aa81f11e2017ee6c49d013328fb8b" />
         <meta name="google-adsense-account" content="ca-pub-6144387361407450"/>
         <title>물타기 계산기</title>
       </Head>
