1c1fcf079876e3f4c39332afa9ec8a66425856f4
Seungwon <dnjs5397@naver.com>
2025-03-27 00:04:54 +0900

    GA-4 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,24 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>물타기 계산기</title>
       </Head>
+
+      {/* ✅ GA4 최신 스크립트: 공식 가이드를 Next.js용으로 변환 */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-87EPDWLH1D"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-87EPDWLH1D');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
