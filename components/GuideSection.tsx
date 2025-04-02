18d308d51a8b64f84aa84d46703740bd28473e92
Seungwon <dnjs5397@naver.com>
2025-04-03 01:18:08 +0900

    guide 영역 렌더링 최적화

--- components/GuideSection.tsx
+++ components/GuideSection.tsx
@@ -2,7 +2,7 @@ export default function GuideSection() {
     return (
       <div className="mt-10 bg-white border border-gray-200 rounded-xl p-6 text-gray-800 text-base leading-relaxed shadow-sm w-full max-w-2xl mx-auto">
         <h3 className="text-lg font-bold mb-3">📘 물타기 계산기 사용 가이드</h3>
-        <p className="mb-3">
+        <p className="mb-3 text-base leading-normal font-normal">
           올라가기만을 기다리는 내 소중한 주식... <br />
           이 계산기는 각 매수 가격과 수량을 입력하면 평균 매입 단가를 자동으로 계산해줍니다. <br />
           또한, 목표 주가를 입력하면 해당 주가까지 얼마나 올라야 하는지(%) 함께 확인하실 수 있어요.
